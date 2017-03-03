6c039fe7c274b8bdc69639fc43487405954d296e
ngkjon <ngkjon@gmail.com>
2017-03-03 12:04:56 -0500

    some js things and tags in body?

--- js/home.js
+++ js/home.js
@@ -1,1 +1,21 @@
-document.getElementById('head').appendChild('<meta property="og:title" content="mashup"/>');
+//alert('hi');
+var body = document.createElement('p');
+
+function getUrlParameter(sParam) {
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) {
+            return sParameterName[1];
+        }
+    }
+};
+var blank = "fire";
+blank = getUrlParameter('header');
+var content = document.createTextNode(blank);
+body.appendChild(content);
+var pos = document.getElementById('guts');
+document.body.insertBefore(body, pos);
+document.getElementById('title').append('meme')
+window.document.title('banjo')
